a7b675eabb2550447af3b89cb5c187d17209ec88
UlfetZakirli <88549805+UlfetZakirli@users.noreply.github.com>
2022-11-06 18:56:26 +0400

    fixed format bug in index.js

--- src/index.js
+++ src/index.js
@@ -2,31 +2,18 @@ import React from 'react';
 import * as ReactDOMClient from "react-dom/client"
 import './index.css';
 import App from './App';
-import {
-  BrowserRouter
-} from 'react-router-dom';
-import {
-  Provider
-} from 'react-redux';
-import {
-  store
-} from './store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot(container)
 
 
-root.render( <
-  React.StrictMode >
-  <
-  BrowserRouter >
-  <
-  Provider store = {
-    store
-  } >
-  <
-  App / >
-  <
-  /Provider> <
-  /BrowserRouter> <
-  /React.StrictMode>
+root.render(<React.StrictMode >
+  <BrowserRouter >
+    <Provider store={store} >
+      < App />
+    </Provider>
+  </BrowserRouter>
+</React.StrictMode>
 );
